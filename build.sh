13299fbaa9ee1c03638f375aaf0931131b1f6c8e
Matyas Somfai <somfai.matyas@netpositive.hu>
2017-12-01 17:51:13 +0100

    build.sh added

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+TAG=${1:-"latest"}
+
+echo "Building base image with tag: $TAG"
+docker build -t netpositivehu/dotdeb-php7.0:$TAG base
+
+echo "Building qatools image with tag: $TAG"
+docker build -t netpositivehu/dotdeb-php7.0-qatools:$TAG qatools
+
+echo "Building staging image with tag: $TAG"
+docker build -t netpositivehu/dotdeb-php7.0-staging:$TAG staging
