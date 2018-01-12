a017925cf1e41e1bac845d7f5910c625a4cb449b
Matyas Somfai <somfai.matyas@netpositive.hu>
2018-01-12 13:45:26 +0100

    don't use docker cache when running the build script

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -e
 TAG=${1:-"latest"}
 
 echo "Building base image with tag: $TAG"
-docker build -t netpositivehu/dotdeb-php7.0:$TAG base
+docker build --no-cache -t netpositivehu/dotdeb-php7.0:$TAG base
 
 echo "Building qatools image with tag: $TAG"
-docker build -t netpositivehu/dotdeb-php7.0-qatools:$TAG qatools
+docker build --no-cache -t netpositivehu/dotdeb-php7.0-qatools:$TAG qatools
 
 echo "Building staging image with tag: $TAG"
-docker build -t netpositivehu/dotdeb-php7.0-staging:$TAG staging
+docker build --no-cache -t netpositivehu/dotdeb-php7.0-staging:$TAG staging
